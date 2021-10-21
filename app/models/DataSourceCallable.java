850d2742f25a57fe7fa4d50b34446d13ec95f6cd
yaa6681 <yaa6681@rit.edu>
2021-10-21 00:48:49 -0400

    Add DataSourceCallable functional interface.

---
+++ app/models/DataSourceCallable.java
@@ -0,0 +1,18 @@
+package models;
+
+import java.sql.Connection;
+
+/**
+ * Functional interface for DataSource Callback.
+ * @author Yaqim Auguste (yaa6681@rit.edu)
+ * @param <T> return type of callback.
+ */
+public interface DataSourceCallable<T> {
+    /**
+     * Execute some code with a database connection.
+     * @param conn The connection object representing a connection to a PostgresSQL database.
+     * @return Any value that the calling class expects.
+     * @throws Exception An exception that is raised when executing the callback.
+     */
+    T call(Connection conn) throws Exception;
+}
