15f1b79a96bbb76ff3b7b777e56233dc9b54f3c0
yaa6681 <yaa6681@rit.edu>
2021-10-25 17:01:08 -0400

    Create MovieCollection value object

---
+++ app/models/MovieCollection.java
@@ -0,0 +1,61 @@
+package models;
+
+/**
+ * Value object for a movie collection
+ * @author Yaqim Auguste (yaa6681@rit.edu)
+ */
+public class MovieCollection {
+    private int collectionID;
+    private String name;
+    private int total;
+    private int length;
+
+    /**
+     * COnstructor for MovieCollection
+     * @param collectionID Collection's ID
+     * @param name Collection's name
+     * @param total Collection's number of movies
+     * @param length Collection's total length
+     */
+    public MovieCollection(int collectionID, String name, int total, int length) {
+        this.collectionID = collectionID;
+        this.name = name;
+        this.total = total;
+        this.length = length;
+    }
+
+    /**
+     * Getter for collectionID
+     */
+    public int getCollectionID() {
+        return collectionID;
+    }
+
+    /**
+     * Getter for name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Getter for total
+     */
+    public int getTotal() {
+        return total;
+    }
+
+    /**
+     * Getter for length
+     */
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     * Getter for length formatted to (hours:minutes)
+     */
+    public String getLengthFormat() {
+        return (int)Math.floor(length/60) + ":" + length%60;
+    }
+}
