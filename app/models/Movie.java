385dd20a59e532b74b5a73c3aaa9e90397bd7716
yaa6681 <yaa6681@rit.edu>
2021-10-30 15:11:24 -0400

    Add hash and equals methods

--- app/models/Movie.java
+++ app/models/Movie.java
@@ -1,11 +1,13 @@
 package models;
 
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * Represents one movie
  *
- * @author Alex lee     al3774@rit.edu
+ * @author Alex lee (al3774@rit.edu)
+ * @author Yaqim Auguste (yaa6681@rit.edu)
  */
 public class Movie {
     private int movieID;
@@ -13,20 +15,42 @@ public class Movie {
     private int length;
     private Date releaseDate;
     private String mpaa;
+    private double rating;
+    private String directors;
+    private String castMembers;
 
     /**
      * Constructor for movie
-     * @param title the title
-     * @param length length of the movie
-     * @param releaseDate date released
-     * @param mpaa rating
+     * @param movieID Movie id of the movie
+     * @param title Title of the movie
+     * @param length Length of the movie
+     * @param releaseDate Release date of the movie
+     * @param mpaa MPAA rating of the movie
+     * @param rating Average user rating of the movie
+     * @param directors Movie's directors
+     * @param castMembers Movie's cast members
      */
-    public Movie(int movieID, String title, int length, Date releaseDate, String mpaa) {
+    public Movie(int movieID, String title, int length, Date releaseDate, String mpaa, double rating, String directors, String castMembers) {
         this.movieID = movieID;
         this.title = title;
         this.length = length;
         this.releaseDate = releaseDate;
         this.mpaa = mpaa;
+        this.rating = rating;
+        this.directors = directors;
+        this.castMembers = castMembers;
+    }
+
+    /**
+     * Constructor for movie independent of derived attributes
+     * @param movieID Movie id of the movie
+     * @param title Title of the movie
+     * @param length Length of the movie
+     * @param releaseDate Release date of the movie
+     * @param mpaa MPAA rating of the movie
+     */
+    public Movie(int movieID, String title, int length, Date releaseDate, String mpaa) {
+        this(movieID, title, length, releaseDate, mpaa, 0, null, null);
     }
 
     /**
@@ -52,6 +76,34 @@ public class Movie {
     public String getMpaa() { return mpaa; }
 
     /**
+     * Gets the movieID
+     */
+    public int getMovieID() {
+        return movieID;
+    }
+
+    /**
+     * Gets the rating
+     */
+    public double getRating() {
+        return rating;
+    }
+
+    /**
+     * Gets the directors
+     */
+    public String getDirectors() {
+        return directors;
+    }
+
+    /**
+     * Gets the cast members
+     */
+    public String getCastMembers() {
+        return castMembers;
+    }
+
+    /**
      * Takes the length in minutes and returns a more readable version in the
      * format: "00hr 00min"
      * @return the formatted string
@@ -61,5 +113,26 @@ public class Movie {
         String minutes = String.valueOf(length % 60);
 
         return hours + "hr " + minutes + "min";
+    }
+
+    /**
+     * Equals method
+     * @param o other object
+     * @return whether the two are equal or not
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return movieID == movie.movieID;
+    }
+
+    /**
+     * Hash method
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(movieID);
     }
 }
